db41cc733d73954b1acf839469aba84e9ca2c16a
Somayaji Ayalasomayajula <somayajiayalasomayajula@MacBook-Pro.local>
2023-01-22 13:27:15 -0500

    Updated logo

--- src/Logo.js
+++ src/Logo.js
@@ -1,39 +1,50 @@
 const Logo = (props) => (
-	<svg className={props.className || "w-24 h-24"} viewBox="0 0 148 170" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<rect x="6.46851" y="79.6786" width="100" height="100" transform="rotate(-45 6.46851 79.6786)" fill="url(#paint0_linear)"/>
-	<circle cx="48.49" cy="47.4901" r="22.9739" transform="rotate(-45 48.49 47.4901)" fill="url(#paint1_linear)"/>
-	<circle cx="129.135" cy="116.135" r="8.58055" transform="rotate(-45 129.135 116.135)" fill="url(#paint2_linear)"/>
-	<rect x="74" y="104" width="48" height="48" transform="rotate(-90 74 104)" fill="url(#paint3_linear)"/>
-	<rect y="57.0001" width="19" height="19" transform="rotate(-90 0 57.0001)" fill="url(#paint4_linear)"/>
-	<path d="M18.4354 134.084L63.0843 89.4355L79.4269 150.427L18.4354 134.084Z" fill="url(#paint5_linear)"/>
-	<path d="M101.652 26.6642L123.664 4.65179L131.721 34.7213L101.652 26.6642Z" fill="#4029B5"/>
-	<defs>
-	<linearGradient id="paint0_linear" x1="56.4685" y1="79.6786" x2="56.4685" y2="179.679" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#7B61FF"/>
-	<stop offset="1" stopColor="#2103BA"/>
-	</linearGradient>
-	<linearGradient id="paint1_linear" x1="48.49" y1="24.5162" x2="48.49" y2="70.464" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#46E1C5"/>
-	<stop offset="1" stopColor="#00836B"/>
-	</linearGradient>
-	<linearGradient id="paint2_linear" x1="129.135" y1="107.554" x2="129.135" y2="124.715" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#46E1C5"/>
-	<stop offset="1" stopColor="#00836B"/>
-	</linearGradient>
-	<linearGradient id="paint3_linear" x1="98" y1="104" x2="98" y2="152" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#FFEC40"/>
-	<stop offset="1" stopColor="#FFB931"/>
-	</linearGradient>
-	<linearGradient id="paint4_linear" x1="9.5" y1="57.0001" x2="9.5" y2="76.0001" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#FFC961"/>
-	<stop offset="1" stopColor="#FFE600"/>
-	</linearGradient>
-	<linearGradient id="paint5_linear" x1="18.4354" y1="134.084" x2="74.5" y2="119" gradientUnits="userSpaceOnUse">
-	<stop stopColor="#4029B5"/>
-	<stop offset="1" stopColor="#130076"/>
-	</linearGradient>
-	</defs>
-  </svg>
+// 	<svg className={props.className || "w-24 h-24"} viewBox="0 0 148 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+// 	<rect x="6.46851" y="79.6786" width="100" height="100" transform="rotate(-45 6.46851 79.6786)" fill="url(#paint0_linear)"/>
+// 	<circle cx="48.49" cy="47.4901" r="22.9739" transform="rotate(-45 48.49 47.4901)" fill="url(#paint1_linear)"/>
+// 	<circle cx="129.135" cy="116.135" r="8.58055" transform="rotate(-45 129.135 116.135)" fill="url(#paint2_linear)"/>
+// 	<rect x="74" y="104" width="48" height="48" transform="rotate(-90 74 104)" fill="url(#paint3_linear)"/>
+// 	<rect y="57.0001" width="19" height="19" transform="rotate(-90 0 57.0001)" fill="url(#paint4_linear)"/>
+// 	<path d="M18.4354 134.084L63.0843 89.4355L79.4269 150.427L18.4354 134.084Z" fill="url(#paint5_linear)"/>
+// 	<path d="M101.652 26.6642L123.664 4.65179L131.721 34.7213L101.652 26.6642Z" fill="#4029B5"/>
+// 	<defs>
+// 	<linearGradient id="paint0_linear" x1="56.4685" y1="79.6786" x2="56.4685" y2="179.679" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#7B61FF"/>
+// 	<stop offset="1" stopColor="#2103BA"/>
+// 	</linearGradient>
+// 	<linearGradient id="paint1_linear" x1="48.49" y1="24.5162" x2="48.49" y2="70.464" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#46E1C5"/>
+// 	<stop offset="1" stopColor="#00836B"/>
+// 	</linearGradient>
+// 	<linearGradient id="paint2_linear" x1="129.135" y1="107.554" x2="129.135" y2="124.715" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#46E1C5"/>
+// 	<stop offset="1" stopColor="#00836B"/>
+// 	</linearGradient>
+// 	<linearGradient id="paint3_linear" x1="98" y1="104" x2="98" y2="152" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#FFEC40"/>
+// 	<stop offset="1" stopColor="#FFB931"/>
+// 	</linearGradient>
+// 	<linearGradient id="paint4_linear" x1="9.5" y1="57.0001" x2="9.5" y2="76.0001" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#FFC961"/>
+// 	<stop offset="1" stopColor="#FFE600"/>
+// 	</linearGradient>
+// 	<linearGradient id="paint5_linear" x1="18.4354" y1="134.084" x2="74.5" y2="119" gradientUnits="userSpaceOnUse">
+// 	<stop stopColor="#4029B5"/>
+// 	<stop offset="1" stopColor="#130076"/>
+// 	</linearGradient>
+// 	</defs>
+//   </svg>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/1999/xlink" width="150" zoomAndPan="magnify" viewBox="0 0 375 374.999991" height="500" preserveAspectRatio="xMidYMid meet" version="1.0">
+	<defs><clipPath id="27523026c7">
+<path d="M 85.242188 85.242188 L 289.992188 85.242188 L 289.992188 289.992188 L 85.242188 289.992188 Z M 85.242188 85.242188 " clip-rule="nonzero"/>
+</clipPath>
+</defs>
+<rect x="-37.5" width="450" fill="none" y="-37.499999" height="449.999989" fill-opacity="1"/>
+<rect x="-37.5" width="450" fill="none" y="-37.499999" height="449.999989" fill-opacity="1"/>
+<g clip-path="url(#27523026c7)">
+	<path fill="#008037" d="M 219.328125 85.242188 C 207.933594 85.242188 197.160156 87.953125 187.621094 92.765625 C 178.082031 87.953125 167.3125 85.242188 155.917969 85.242188 C 116.949219 85.242188 85.242188 116.949219 85.242188 155.917969 L 85.242188 219.328125 C 85.242188 258.296875 116.949219 290 155.917969 290 L 219.328125 290 C 258.296875 290 290 258.296875 290 219.328125 L 290 155.917969 C 290 116.949219 258.296875 85.242188 219.328125 85.242188 Z M 99.773438 155.917969 C 99.773438 124.960938 124.957031 99.773438 155.917969 99.773438 C 161.917969 99.773438 167.695312 100.726562 173.121094 102.480469 C 159.785156 114.03125 150.800781 130.472656 148.992188 148.992188 C 128.976562 150.949219 111.382812 161.289062 99.777344 176.449219 L 99.777344 155.917969 Z M 211.574219 148.652344 L 163.671875 148.652344 C 165.773438 132.476562 174.769531 118.445312 187.621094 109.613281 C 200.472656 118.445312 209.476562 132.476562 211.574219 148.652344 Z M 187.621094 265.628906 C 172.878906 255.5 163.183594 238.527344 163.183594 219.328125 L 163.183594 163.183594 L 212.0625 163.183594 L 212.0625 219.328125 C 212.0625 238.527344 202.367188 255.5 187.621094 265.628906 Z M 99.773438 219.328125 C 99.773438 190.835938 121.113281 167.242188 148.652344 163.671875 L 148.652344 219.328125 C 148.652344 240.648438 158.140625 259.792969 173.121094 272.761719 C 167.699219 274.511719 161.917969 275.472656 155.917969 275.472656 C 124.960938 275.472656 99.773438 250.28125 99.773438 219.328125 Z M 219.328125 275.472656 C 213.328125 275.472656 207.550781 274.515625 202.121094 272.761719 C 217.101562 259.792969 226.589844 240.648438 226.589844 219.328125 L 226.589844 163.671875 C 254.125 167.246094 275.472656 190.835938 275.472656 219.328125 C 275.472656 250.28125 250.28125 275.472656 219.328125 275.472656 Z M 226.253906 148.992188 C 224.441406 130.472656 215.464844 114.03125 202.121094 102.480469 C 207.546875 100.730469 213.328125 99.773438 219.328125 99.773438 C 250.28125 99.773438 275.472656 124.957031 275.472656 155.917969 L 275.472656 176.449219 C 263.867188 161.289062 246.273438 150.949219 226.253906 148.992188 Z M 226.253906 148.992188 " fill-opacity="1" fill-rule="nonzero"/>
+	</g>
+	</svg>
   )
 
   export default Logo
