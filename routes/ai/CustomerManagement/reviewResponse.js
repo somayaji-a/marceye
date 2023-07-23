1382174a03817d82bbab99e1037fff2abd7af94f
Somayaji Ayalasomayajula <somayaji.ayalasomayajula@npxinnovation.ca>
2023-07-23 17:11:09 -0400

    updated reviewResponse

--- routes/ai/CustomerManagement/reviewResponse.js
+++ routes/ai/CustomerManagement/reviewResponse.js
@@ -8,26 +8,23 @@ app.post('/CustomerManagement/reviewResponse', async (req, res, next) => {
 	try {
 		let { review } = req.body
 
-	let prompt = `You are the sole owner of a shop on Etsy responding to customer reviews on your products. Write a 50 word response to a review that was received on your ecommerce store from a purchaser. The review may be positive or negative. Please thank the purchaser for their feedback, address any concerns they may have mentioned, and offer solutions to any issues they may have encountered. Express your appreciation for their business and encourage them to shop with you again in the future. Make the tone conversational and semi-formal. Do not suggest a refund, but offer to address any specific concerns in a direct message. If the review is hostile, respond in a neutral and disarming tone. Do not refer to any pictures. The review provided was: `
+	let prompt = ``
 
 	let inputRaw = `\n${review}\n\n###\n\n`
 	prompt += inputRaw
 
-	const gptResponse = await openai.createCompletion({
-		model: 'text-davinci-003',
-		prompt,
-		max_tokens: 200,
-		temperature: 0.5,
-		top_p: 1,
+	const gptResponse = await openai.createChatCompletion({
+		model: 'gpt-4',
+		messages: [{"role": "system", "content": "You are an e-commerce AI assistant that helps users generate responses to customer reviews on your products. Write a 50 word response to a review that was received on the ecommerce store from a purchaser. The review may be positive or negative. Please thank the purchaser for their feedback, address any concerns they may have mentioned, and offer solutions if they have encountered any issues. Express your appreciation for their business and encourage them to shop with you again in the future. Make the tone conversational and semi-formal. Do not suggest a refund, but offer to address any specific concerns in a direct message. If the review is hostile, respond in a neutral and disarming tone. Do not refer to any pictures. \n\n"}, {"role":"user", "content": prompt}],
 		frequency_penalty: 1,
 		presence_penalty: 1,
 		n: 1,
 		user: req.user._id,
 		stream: false,
-		stop: ["###", "<|endoftext|>"],
+		stop: ["###", "<|endoftext|>", ],
 	});
 
-	let output = `${gptResponse.data.choices[0].text}`
+	let output = `${gptResponse.data.choices[0].message.content}`
 
 	// // remove the first character from output
 	// output = output.substring(1, output.length)
