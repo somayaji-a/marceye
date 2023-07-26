0f64603123f58490b0a163b7cb3563a523c46385
Somayaji Ayalasomayajula <somayaji.ayalasomayajula@npxinnovation.ca>
2023-07-25 20:26:19 -0400

    working landing page

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,71 +1,14 @@
-// import React from 'react';
-
-// const LandingPage = () => {
-//   return (
-//     <div className="flex flex-col justify-center items-center">
-//       {/* Hero Section */}
-//       <section className="text-center p-10">
-//         <h1 className="text-4xl font-bold mb-4">Welcome to Our SaaS App!</h1>
-//         <p className="text-lg">Your solution for better management.</p>
-//       </section>
-
-//       {/* Feature Section */}
-//       <section className="p-10 grid grid-cols-1 md:grid-cols-3 gap-4">
-//         <div className="feature">
-//           <h2 className="text-2xl font-bold mb-2">Feature 1</h2>
-//           <p>Description for feature 1</p>
-//         </div>
-//         <div className="feature">
-//           <h2 className="text-2xl font-bold mb-2">Feature 2</h2>
-//           <p>Description for feature 2</p>
-//         </div>
-//         <div className="feature">
-//           <h2 className="text-2xl font-bold mb-2">Feature 3</h2>
-//           <p>Description for feature 3</p>
-//         </div>
-//       </section>
-
-//       {/* Pricing Section */}
-//       <section className="p-10 grid grid-cols-1 md:grid-cols-3 gap-4">
-//         <div className="pricing">
-//           <h2 className="text-2xl font-bold mb-2">Basic Plan</h2>
-//           <p>$10/month</p>
-//         </div>
-//         <div className="pricing">
-//           <h2 className="text-2xl font-bold mb-2">Pro Plan</h2>
-//           <p>$20/month</p>
-//         </div>
-//         <div className="pricing">
-//           <h2 className="text-2xl font-bold mb-2">Enterprise Plan</h2>
-//           <p>Contact us for pricing</p>
-//         </div>
-//       </section>
-
-//       {/* Testimonial Section */}
-//       <section className="p-10">
-//         <h2 className="text-2xl font-bold mb-4">What our customers say</h2>
-//         <blockquote className="border-l-4 border-gray-500 pl-4 text-lg italic">
-//           <p>"This is the best SaaS app I've ever used!"</p>
-//           <cite>- Happy Customer</cite>
-//         </blockquote>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default LandingPage;
-
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import { MenuIcon, XIcon  } from '@heroicons/react/outline';
-import { CloudUploadIcon, LockClosedIcon, DatabaseIcon  } from '@heroicons/react/solid';
-
+import { CloudUploadIcon, LockClosedIcon, DatabaseIcon, CheckIcon  } from '@heroicons/react/solid';
+import { NavLink } from 'react-router-dom'
+import Logo from '../Logo.js'
 
 const navigation = [
-  { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Pricing', href: '#' },
+  { name: 'Testimonials', href: '#' },
 ];
 
 const features = [
@@ -87,23 +30,26 @@ const features = [
   },
 ];
 
+const includedFeatures = [
+  'Private forum access',
+  'Member resources',
+  'Entry to annual conference',
+  'Official member t-shirt',
+];
+
 const LandingPage = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="">
       {/* Hero Section */}
       <div className="bg-white">
         <header className="absolute inset-x-0 top-0 z-50">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <span className="sr-only">DeGeneratr</span>
+                <Logo />
               </a>
             </div>
             <div className="flex lg:hidden">
@@ -124,7 +70,7 @@ const LandingPage = () => {
               ))}
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
                 Log in <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
@@ -134,12 +80,8 @@ const LandingPage = () => {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <span className="sr-only">DeGeneratr</span>
+                <Logo />
               </a>
               <button
                 type="button"
@@ -165,7 +107,7 @@ const LandingPage = () => {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
@@ -238,7 +180,7 @@ const LandingPage = () => {
   
 
       {/* Feature Section */}
-      <section className="p-10 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <section className="">
         <div className="overflow-hidden bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
@@ -276,14 +218,109 @@ const LandingPage = () => {
       </section>
 
       {/* Pricing Section */}
-      <section className="p-10 grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* Rest of the code */}
+      <section >
+        {/* className="p-10 grid grid-cols-1 md:grid-cols-3 gap-4" */}
+        <div className="bg-white py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-tricks pricing</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
+            in. Explicabo id ut laborum.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8 sm:p-10 lg:flex-auto">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+              repellendus etur quidem assumenda.
+            </p>
+            <div className="mt-10 flex items-center gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
+              <div className="h-px flex-auto bg-gray-100" />
+            </div>
+            <ul
+              role="list"
+              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+              {includedFeatures.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon className="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="mx-auto max-w-xs px-8">
+                <p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+                <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+                </p>
+                <a
+                  href="#"
+                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get access
+                </a>
+                <p className="mt-6 text-xs leading-5 text-gray-600">
+                  Invoices and receipts available for easy company reimbursement
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
       </section>
 
       {/* Testimonial Section */}
-      <section className="p-10">
-        {/* Rest of the code */}
+      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+          <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
+          <figure className="mt-10">
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <img
+                className="mx-auto h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div className="font-semibold text-gray-900">Judith Black</div>
+                <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <div className="text-gray-600">CEO of Workcation</div>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
       </section>
+
+      <footer>
+      <div className="bg-gray-800 text-white px-6 py-4">
+        <div className="container mx-auto flex flex-wrap justify-between items-center">
+          <div className="flex items-center text-gray-100">
+            <Logo />
+          </div>
+          <div className="mt-4 lg:mt-0">
+            <a href="/about" className="text-gray-400 hover:text-gray-200 mr-6">About Us</a><br/>
+            <a href="/privacy-policy" className="text-gray-400 hover:text-gray-200">Privacy Policy</a>
+          </div>
+          <p className="mt-5 text-gray-400 text-sm sm:mt-0 sm:text-right">© 2023 DeGeneratr. All rights reserved.</p>
+        </div>
+      </div>
+      </footer>
     </div>
   );
 };
